c61586b53971e55bdfad810e9299b80ec41d7822
Marki <minokuchi@gmail.com>
2017-11-13 14:00:39 -0800

    added declare.js file

---
+++ WebFundamentals/Javascript/declaration/declare.js
@@ -0,0 +1,33 @@
+// I Do Declare!
+// Declare variables of the following types:
+//     - 4 Numbers
+//     - 3 Strings
+//     - 2 Booleans
+//     - 1 Undefined Variable
+// Give the variables descriptive names (ex: string1 and string2)
+// Print each one to the console with a description (ex: console.log("The First String:", string1);)
+// Try to be creative! (don't use 1, 2, 3, and 4 for your numbers -- test out negatives or decimals!)   
+// execute this file by typing the following from a terminal widow: "node <file nama>" ex. "node declare.js" 
+
+var number1 = 5;
+var number2 = .008;
+var number3 = 0;
+var number4 = -7;
+var string1 = "Court"
+var string2 = "Avenue"
+var string3 = "Road"
+var house = true;
+var nohouse = false;
+var undef;
+
+console.log("The first number variable is:", number1);
+console.log("The second number variable is:", number2);
+console.log("The third number variable is:", number3);
+console.log("The fourth number variable is:", number4);
+console.log("The first string variable is:", string1);
+console.log("The second string variable is:", string2);
+console.log("The third string variable is:", string3);
+console.log("The first boolean value is:", house);
+console.log("The second boolean variable is:", nohouse);
+console.log("The undef variable is:", );
+
