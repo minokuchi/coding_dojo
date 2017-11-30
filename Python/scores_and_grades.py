6034ae73355e76d557ad3ac88463f64ca47bab57
Marki <minokuchi@gmail.com>
2017-11-30 11:31:21 -0800

    Added scores_and_grade.py

---
+++ Python/scores_and_grades.py
@@ -0,0 +1,20 @@
+import random
+
+def grade(iter):
+    print "Scores and Grades"
+    for i in range(0, iter):
+        score = random.randint(60, 101)
+        if score >= 60 and score <= 69:
+            print "Score: ", score,"; Your grade is D"
+        elif score >= 70 and score <= 79:
+            print "Score: ", score, "; Your grade is C"
+        elif score >= 80 and score <= 89:
+            print "Score: ", score, "; Your grade is B"
+        elif score >= 90 and score <= 100:
+            print "Score: ", score, "; Your grade is A"
+        else:
+            print "You failed"
+    print "End of the program.  Bye!"
+
+grade(10)
+
